2b6aa8a7d9c43249822babb9da0f6d52c2f468b7
rudenokart <rudenokart@yandex.ru>
2022-01-19 15:26:09 +0300

    bitrix - date filter

--- Bitrix.php
+++ Bitrix.php
@@ -12,6 +12,18 @@ class Bitrix {
     }
     return $arr;
   }
+
+  public static function date_filter () {
+    $dateFrom = new \Bitrix\Main\Type\DateTime();
+    $dateFrom->add('-10 day');
+    $dateTo = new \Bitrix\Main\Type\DateTime();
+    $filter = [
+        ">=CLOSED_DATE" => $dateFrom,
+        "<=CLOSED_DATE" => $dateTo,
+        'RESPONSIBLE_ID' => $value['ID'],
+      ];
+      $src = CTasks::GetList([],$filter, []);
+  }
   
 }
 
