ca2ad057bd68f94a29cdec64bec197f0c0647751
rudenokart <rudenokart@yandex.ru>
2022-02-04 09:10:33 +0300

    bitrix agent

--- Bitrix.php
+++ Bitrix.php
@@ -3,13 +3,28 @@
 /**
  * 
  */
+
+
+
 class Bitrix {
+
+  public static function agentCreate () { // Создать агента
+    CAgent::AddAgent(
+    "WorkReport::reportLogging();", // имя функции
+    "", // идентификатор модуля
+    "N", // агент не критичен к кол-ву запусков
+    60, // интервал запуска - 1 сутки
+    "03.02.2022 16:30:00", // дата первой проверки на запуск
+    "Y", // агент активен
+    "03.02.2022 16:30:00", // дата первого запуска
+    "");
+  }
 
   public static function js_library () { // js библиотека
     CJSCore::RegisterExt('Panel_visability_js', array(
-    'js' => '/local/gadgets/custom/panel_visability/main.js',
-  ));
-  CUtil::InitJSCore(array('Panel_visability_js'));
+      'js' => '/local/gadgets/custom/panel_visability/main.js',
+    ));
+    CUtil::InitJSCore(array('Panel_visability_js'));
   }
 
   public static function list_maker($src) {
@@ -25,11 +40,11 @@ class Bitrix {
     $dateFrom->add('-10 day');
     $dateTo = new \Bitrix\Main\Type\DateTime();
     $filter = [
-        ">=CLOSED_DATE" => $dateFrom,
-        "<=CLOSED_DATE" => $dateTo,
-        'RESPONSIBLE_ID' => $value['ID'],
-      ];
-      $src = CTasks::GetList([],$filter, []);
+      ">=CLOSED_DATE" => $dateFrom,
+      "<=CLOSED_DATE" => $dateTo,
+      'RESPONSIBLE_ID' => $value['ID'],
+    ];
+    $src = CTasks::GetList([],$filter, []);
   }
   
 }
