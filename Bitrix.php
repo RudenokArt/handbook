c7451adbad4ad77960fc85659c273d2b3091d96e
rudenokart <rudenokart@yandex.ru>
2022-02-15 09:36:37 +0300

    'bitrix'

--- Bitrix.php
+++ Bitrix.php
@@ -19,19 +19,19 @@ function list_maker($src) {
   return $arr;
 }
 
-  // ========== JS LIBRARY ==========
+// ========== JS LIBRARY ==========
 
-   function js_library () { // js библиотека
-    CJSCore::RegisterExt('Panel_visability_js', array(
-      'js' => '/local/gadgets/custom/panel_visability/main.js',
-    ));
-    CUtil::InitJSCore(array('Panel_visability_js'));
-  }
+function js_library () { // js библиотека
+  CJSCore::RegisterExt('Panel_visability_js', array(
+    'js' => '/local/gadgets/custom/panel_visability/main.js',
+  ));
+  CUtil::InitJSCore(array('Panel_visability_js'));
+}
 
-  // ========== AGETNS ==========
+// ========== AGETNS ==========
 
-  function agentCreate () { // Создать агента
-    CAgent::AddAgent(
+function agentCreate () { // Создать агента
+  CAgent::AddAgent(
     "WorkReport::reportLogging();", // имя функции
     "", // идентификатор модуля
     "N", // агент не критичен к кол-ву запусков
@@ -40,13 +40,13 @@ function list_maker($src) {
     "Y", // агент активен
     "03.02.2022 16:30:00", // дата первого запуска
     "");
-  }
+}
 
-  // ========== CRM LEADS ==========
+// ========== CRM LEADS ==========
 
-  function get_leads_sourses () {  // получить источники лидов
-    CCrmStatus::GetStatusList('SOURCE');
-  }
+function get_leads_sourses () {  // получить источники лидов
+  CCrmStatus::GetStatusList('SOURCE');
+}
 
 function getSourceList () { // получить источники лидов через rest
   $str = file_get_contents('https://crm.maunfeld.by/rest/10/shdvcx5dj3zd289m/crm.status.entity.items.json?entityId=SOURCE');
@@ -61,7 +61,7 @@ function getSourceList () { // получить источники лидов ч
   return $list;
 }
 
-  // ========== DATE & TIME ==========
+// ========== DATE & TIME ==========
 
 function date_filter () {
   $dateFrom = new \Bitrix\Main\Type\DateTime();
