d89ef490716aef4e164b51d4732d8ed19319cc3f
RudenokArt <rudenokart@yandex.ru>
2022-12-09 09:01:04 +0300

    sql

--- MySql.sql
+++ MySql.sql
@@ -1,3 +1,4 @@
 -- Добавить внешний ключ
 ALTER TABLE posts ADD FOREIGN KEY(category_id) REFERENCES categories(id); 
 ALTER TABLE comments ADD FOREIGN KEY(post_id) REFERENCES posts(id);
+ALTER TABLE comments ADD FOREIGN KEY(user) REFERENCES posts(id);
