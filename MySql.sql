ed1769d3a9c507f2b8b3c29c8b8daa608f4f88f7
RudenokArt <rudenokart@yandex.ru>
2022-12-19 12:34:48 +0300

    MySql

--- MySql.sql
+++ MySql.sql
@@ -1,4 +1,7 @@
--- Добавить внешний ключ
+Добавить внешний ключ
 ALTER TABLE posts ADD FOREIGN KEY(category_id) REFERENCES categories(id); 
 ALTER TABLE comments ADD FOREIGN KEY(post_id) REFERENCES posts(id);
 ALTER TABLE comments ADD FOREIGN KEY(user) REFERENCES posts(id);
+
+Удалить внешний ключ
+alter table posts drop constraint posts_ibfk_1
