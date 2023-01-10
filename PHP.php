3c161aefb8b66fea92cac8522e7276d98c32c92a
RudenokArt <rudenokart@yandex.ru>
2023-01-10 08:21:39 +0300

    php

--- PHP.php
+++ PHP.php
@@ -97,29 +97,6 @@ function cUrlPost () { // cUrl - запрос методом POST
 }
 
 
-// Простая пагинация: на входе массив статей и количетво на странице
-// на выходе - массив статей на странице и постраничное меню
-function pagination ($arr, $page_size) {
- $current_page = 1;
- if (isset($_GET['page'])) {
-  $current_page = $_GET['page'];
-}
-$last_item = $current_page*$page_size-1;
-$first_item = $last_item - $page_size+1;
-$page = [];
-for ($i=$first_item; $i <= $last_item ; $i++) { 
-  array_push($page, $arr[$i]);
-}
-$nav = [];
-for ($i=0; $i < count($arr)/$page_size; $i++) { 
-  $n = $i+1;
-  $page_nuber = $n;
-  $page_link = '?page='.$n;
-  array_push($nav, ['page_nuber' => $page_nuber, 'page_link' => $page_link]);
-}
-return ['page'=>$page, 'nav'=>$nav];
-}
-
 function date_interval () { // выбор интервала дат
   if (isset($_POST['filter'])) {
     if ($_POST['date_filter']=='month') {
