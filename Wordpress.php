155bffe66e2e41a44b7e16380a1d57d35d9b70d6
RudenokArt <rudenokart@yandex.ru>
2023-01-26 22:54:55 +0300

    wp

--- Wordpress.php
+++ Wordpress.php
@@ -7,23 +7,36 @@ require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 $sql=$wpdb->get_results('SELECT `post_content` FROM `wp_posts`');
 print_r($sql);
 
-// Добавить страницу к админке WP
+// ===== ДОБАВЛЕНИЕ СВОИХ СТРАНИЦ В АДМИНКУ
 add_action( 'admin_menu', 'true_top_menu_page', 25 );
 function true_top_menu_page(){
- 
 	add_menu_page(
 		'Настройки конструктора', // тайтл страницы
 		'Настройки конструктора', // текст ссылки в меню
 		'manage_options', // права пользователя, необходимые для доступа к странице
-		'constructor_options', // ярлык страницы
-		'constructor_options_manager', // функция, которая выводит содержимое страницы
+		'ug_ideal_admin', // ярлык страницы
+		'ug_ideal_admin', // функция, которая выводит содержимое страницы
 		'dashicons-admin-generic', // иконка, в данном случае из Dashicons
 		7 // позиция в меню
 	);
+
+	add_submenu_page(
+		'ug_ideal_admin',
+		'Шаблоны - размеры и цены', // тайтл страницы
+		'Шаблоны - размеры и цены', // текст ссылки в меню
+		'manage_options', // права пользователя, необходимые для доступа к странице
+		'ug_ideal_admin_template_size', // ярлык страницы
+		'ug_ideal_admin_template_size' // функция, которая выводит содержимое страницы
+	);
 } 
-function constructor_options_manager () {
-	echo 'привет';
+function ug_ideal_admin () {
+	include_once 'ug_ideal-includes/admin.php';
 }
+function ug_ideal_admin_template_size(){
+	include_once 'ug_ideal-includes/admin-template-size.php';
+}
+// ==========================================================
+
 
 ?>
 
