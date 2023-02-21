5872b3181a31e3e3da1d95b5f744e2ac135aeaa7
RudenokArt <rudenokart@yandex.ru>
2023-02-21 17:16:25 +0300

    sql

--- MySql.sql
+++ MySql.sql
@@ -1,3 +1,5 @@
+Добавить индекс
+ALTER TABLE `products` ADD INDEX(`category_id`);
 Добавить внешний ключ
 ALTER TABLE posts ADD FOREIGN KEY(category_id) REFERENCES categories(id); 
 ALTER TABLE comments ADD FOREIGN KEY(post_id) REFERENCES posts(id);
