429f01e28dd8830bdc7479c016578f7daee7ce90
RudenokArt <rudenokart@yandex.ru>
2023-02-23 10:30:21 +0300

    sql

--- MySql.sql
+++ MySql.sql
@@ -1,9 +1,17 @@
-Добавить индекс
+-- Добавить индекс
 ALTER TABLE `products` ADD INDEX(`category_id`);
 Добавить внешний ключ
 ALTER TABLE posts ADD FOREIGN KEY(category_id) REFERENCES categories(id); 
 ALTER TABLE comments ADD FOREIGN KEY(post_id) REFERENCES posts(id);
 ALTER TABLE comments ADD FOREIGN KEY(user) REFERENCES posts(id);
 
-Удалить внешний ключ
+-- Удалить внешний ключ
 alter table posts drop constraint posts_ibfk_1
+
+-- JOIN один ко многим
+SELECT * FROM `products` INNER JOIN `categories` ON `products`.`category_id`=`categories`.`id`
+
+-- Многие ко многим
+SELECT * FROM `products_tags` 
+INNER JOIN `products` ON `products_tags`.`product_id`=`products`.`id`
+INNER JOIN `tags` ON `products_tags`.`tag_id`=`tags`.`id`
