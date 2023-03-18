70649a3548bf8789edc9ff8043a499bbe3dea18c
RudenokArt <rudenokart@yandex.ru>
2023-03-18 22:58:53 +0300

    bitrix

--- Bitrix.php
+++ Bitrix.php
@@ -134,6 +134,14 @@ $hl_create = Bitrix\Highloadblock\HighloadBlockTable::add(array(
   'TABLE_NAME' => 'tasks_premium', 
 ));
 
+// Удалить highload по символьному коду
+$hl_get = \Bitrix\Highloadblock\HighloadBlockTable::getList([
+  'filter'=>['TABLE_NAME' => 'tasks_premium',],
+])->Fetch()['ID'];
+if ($hl_get) {
+  \Bitrix\Highloadblock\HighloadBlockTable::delete($hl_get);
+}
+
 // получить highload-блок по фильтру
 $hl_get = \Bitrix\Highloadblock\HighloadBlockTable::getList([
   'filter'=>['TABLE_NAME' => 'ts_prices',],
@@ -377,29 +385,29 @@ function reportFilterDate () {
 
  <?php // BITRIX CAPTHA
  include_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/classes/general/captcha.php");
-    $cpt = new CCaptcha();
-    $captchaPass = COption::GetOptionString("main", "captcha_password", "");
-    if(strlen($captchaPass) <= 0) {
-      $captchaPass = randString(10);
-      COption::SetOptionString("main", "captcha_password", $captchaPass);
-    }
-    $cpt->SetCodeCrypt($captchaPass);
-    ?>
-    <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-12 mice-event-title">
-        * Введите код с картинки
-        <input value="<?=htmlspecialchars($cpt->GetCodeCrypt());?>" 
-        class="captchaSid" name="captcha_code" type="hidden">
-      </div>
-      <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-        <input class="inptext form-control" required="" id="captcha_word" name="captcha_word" type="text">
-      </div>
-      <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-        <img class="captchaImg" 
-        src="/bitrix/tools/captcha.php?captcha_code=<?=htmlspecialchars($cpt->GetCodeCrypt());?>">
-      </div>
-    </div>
-    <pre><?php print_r($cpt->code);?></pre>
+ $cpt = new CCaptcha();
+ $captchaPass = COption::GetOptionString("main", "captcha_password", "");
+ if(strlen($captchaPass) <= 0) {
+  $captchaPass = randString(10);
+  COption::SetOptionString("main", "captcha_password", $captchaPass);
+}
+$cpt->SetCodeCrypt($captchaPass);
+?>
+<div class="row">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-12 mice-event-title">
+    * Введите код с картинки
+    <input value="<?=htmlspecialchars($cpt->GetCodeCrypt());?>" 
+    class="captchaSid" name="captcha_code" type="hidden">
+  </div>
+  <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+    <input class="inptext form-control" required="" id="captcha_word" name="captcha_word" type="text">
+  </div>
+  <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+    <img class="captchaImg" 
+    src="/bitrix/tools/captcha.php?captcha_code=<?=htmlspecialchars($cpt->GetCodeCrypt());?>">
+  </div>
+</div>
+<pre><?php print_r($cpt->code);?></pre>
 
 
 <script> // преопределить битриксовый прелодер
