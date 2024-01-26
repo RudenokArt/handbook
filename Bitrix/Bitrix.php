142ea9de48d7402f19616c2db65ddb4d273da4e2
RudenokArt <rudenokart@yandex.ru>
2024-01-26 16:48:08 +0300

    save

--- Bitrix/Bitrix.php
+++ Bitrix/Bitrix.php
@@ -46,30 +46,6 @@ $this->getPath();
 // получить список стран
 print_r(GetCountryArray(LANGUAGE_ID));
 
-// Регистрация обработчика события
-\Bitrix\Main\Loader::includeModule('crm');
-AddEventHandler('crm', 'OnAfterCrmDealUpdate', 'LocalEventHandler::OnAfterCrmDealUpdate');
-class LocalEventHandler {
-  function OnAfterCrmDealUpdate ($arr) {
-    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/test/log.json', json_encode($arr));
-  } 
-}
-
-// Регистрация обработчика события при установке модуля
-registerModuleDependences('documentgenerator', 'onBeforeProcessDocument', $this->MODULE_ID, 'DocumentGeneratorHandler', 'customizeDocument');
-class DocumentGeneratorHandler {
-  public static function customizeDocument($event)
-  { 
-    $event->setValues(['signature' => '${signature}']);
-    // // // добавить дополнительные описания полей
-    // // // $document->setFields($newFields);
-    // // // добавить значения полей
-    // $document->setValues(['signature' => '${signature}']);
-    // // // получить список полей и их текущих значений
-    // // //$fields = $document->getFields();
-  }
-}
-
 // ===== Конвертировать BB коды в html ======
 $bbTextParser = new CTextParser();
 $messages[$key]['MESSAGE'] = $bbTextParser->convertText($value['MESSAGE']);
---
+++ Bitrix/Events.php
@@ -0,0 +1,47 @@
+<?php
+
+// Регистрация обработчика события
+\Bitrix\Main\Loader::includeModule('crm');
+AddEventHandler('crm', 'OnAfterCrmDealUpdate', 'LocalEventHandler::OnAfterCrmDealUpdate');
+class LocalEventHandler {
+  function OnAfterCrmDealUpdate ($arr) {
+    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/test/log.json', json_encode($arr));
+  } 
+}
+
+// Регистрация обработчика события при установке модуля
+registerModuleDependences('documentgenerator', 'onBeforeProcessDocument', $this->MODULE_ID, 'DocumentGeneratorHandler', 'customizeDocument');
+class DocumentGeneratorHandler {
+  public static function customizeDocument($event)
+  { 
+    $event->setValues(['signature' => '${signature}']);
+    // // // добавить дополнительные описания полей
+    // // // $document->setFields($newFields);
+    // // // добавить значения полей
+    // $document->setValues(['signature' => '${signature}']);
+    // // // получить список полей и их текущих значений
+    // // //$fields = $document->getFields();
+  }
+}
+// Добавление вкладки в crm сущность (сделку)ж
+Bitrix\Main\EventManager::getInstance()->addEventHandler(
+  'crm',
+  'onEntityDetailsTabsInitialized', 
+  static function(\Bitrix\Main\Event $event) {
+    $entityId = $event->getParameter('entityID');
+    $tabs = $event->getParameter('tabs');
+    $tabs[] = [
+      'id' => 'crm_products_calc',
+      'name' => 'Products(calc)',
+      'loader' => [
+        'serviceUrl' => '/local/components/docbrown/crm_products_calc/lazyload.ajax.php',
+        'componentData' => [
+          'dealId' => $entityId
+        ]
+      ]
+    ];
+    return new \Bitrix\Main\EventResult(\Bitrix\Main\EventResult::SUCCESS, [
+      'tabs' => $tabs,
+    ]);
+  }
+);
