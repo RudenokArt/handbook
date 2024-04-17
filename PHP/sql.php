8b829dc70357638f0e634a34933e40f76a8f0450
RudenokArt <RudenokArt@yandex.ru>
2024-04-17 08:30:35 +0300

    php

--- PHP/sql.php
+++ PHP/sql.php
@@ -1,14 +1,14 @@
-<?php 
-$host='localhost';
-$log='root';
-$pas='';
-$db='pladas';
-$link=mysqli_connect($host,$log,$pas,$db);
-$src=mysqli_query($link,'SHOW TABLES FROM pladas');
+<?php
+$host ='localhost';
+$log ='root';
+$pas ='';
+$db ='family-camp';
+$link = mysqli_connect($host,$log,$pas,$db);
+$src = mysqli_query($link,'SELECT * FROM wp_posts');
 while ($row = mysqli_fetch_assoc($src)) {
         $arr[] = $row;
 }
 
 ?>
-
+<pre><?php print_r($src); ?></pre>
 <pre><?php print_r($arr); ?></pre>
