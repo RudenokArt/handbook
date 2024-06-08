ba367ff63cd97367174ba2ff8e6d3926db1f9692
RudenokArt <RudenokArt@yandex.ru>
2024-06-08 11:47:27 +0300

    save

--- Wordpress/Wordpress.php
+++ Wordpress/Wordpress.php
@@ -8,29 +8,40 @@
 
 // https://underscores.me/ Генератор тем
 
+
 // Подключить пролог без шапки
- require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
+require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
+
 
 // Включить поддержку миниатюр
- add_theme_support( 'post-thumbnails' );
-
+add_theme_support( 'post-thumbnails' );
 // Включить поддержку логотипа
- add_action('after_setup_theme', static function () {
- 	add_theme_support('custom-logo', []);
- });
-
- // Поддержка title
- add_theme_support('title_tag');
-
+add_action('after_setup_theme', static function () {
+	add_theme_support('custom-logo', []);
+});
+// Поддержка title
+add_theme_support('title_tag');
 // Поддержка меню
- add_theme_support('menus');
+add_theme_support('menus')
+// Отключить админ-бар в публичной части
+add_filter( 'show_admin_bar', '__return_false' );
 
 
 // Получить URL логотипа:
  wp_get_attachment_url(get_theme_mod('custom_logo'));
-
 // Получить favicon
  echo  get_site_icon_url();
+
+// подключение скриптов и стилей
+add_action( 'wp_enqueue_scripts', static function () {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_script('jquery');
+	wp_enqueue_style('bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+	wp_enqueue_script('bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', [], false, ['in_footer' => true]);
+	wp_enqueue_script( 'script_js', get_template_directory_uri() . '/js/script.js');
+	wp_enqueue_style( 'style_css', get_stylesheet_directory_uri() . '/css/style.css' );
+});
+
 
 // Подключение файла php
  get_template_part('core/AdminRouter');
@@ -50,8 +61,8 @@
 
 
 // SQL запрос к БД
-$sql=$wpdb->get_results('SELECT `post_content` FROM `wp_posts`');
-print_r($sql);
+ $sql=$wpdb->get_results('SELECT `post_content` FROM `wp_posts`');
+ print_r($sql);
 
 
 
