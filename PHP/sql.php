e929fc667811fbca123f4c4c6240f059cd892c7e
RudenokArt <RudenokArt@yandex.ru>
2024-11-28 16:28:51 +0300

    save

--- PHP/sql.php
+++ PHP/sql.php
@@ -1,14 +1,17 @@
 <?php
-$host ='localhost';
-$log ='root';
-$pas ='';
+$host = 'localhost';
+$log = 'root';
+$pas = '';
 $db ='family-camp';
-$link = mysqli_connect($host,$log,$pas,$db);
+$link = mysqli_connect($host, $log, $pas, $db);
 mysqli_query($link, "SET NAMES 'utf8'");
 $src = mysqli_query($link,'SELECT * FROM wp_posts');
 while ($row = mysqli_fetch_assoc($src)) {
         $arr[] = $row;
 }
+
+// Экранировка кавычек и "проблемных" символов ИЗ ФОРМЫ
+$login = mysqli_real_escape_string($link, $_REQUEST['login']);
 
 // Количество записей
 $src = mysqli_query($link,'SELECT COUNT(*) FROM wp_posts');
