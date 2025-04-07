a185b7376030cdbc79b45f1593ca4322d13e3ff5
RudenokArt <RudenokArt@yandex.ru>
2025-04-07 15:22:11 +0300

    bitrix sale

---
+++ Bitrix/Sale.php
@@ -0,0 +1,33 @@
+<?php
+
+// Событие перед сохранением корзины, 
+\Bitrix\Main\EventManager::getInstance()->addEventHandler(
+	'sale',
+	'OnSaleBasketBeforeSaved',
+	function (\Bitrix\Main\Event $event) {
+		$basket = $event->getParameter("ENTITY");
+		// \Bitrix\Main\Diag\Debug::writeToFile($arr, 'arr', 'local/tests/test.log');
+	}
+);
+
+// получение позиций корзины
+$basketItems = $basket->getBasketItems();
+foreach ($basketItems as $basketItem) {
+	$arr[] = [
+		'NAME' => $basketItem->getField('NAME'),
+		'PRICE' => $basketItem->getPrice(),
+		'QUANTITY' =>  $basketItem->getQuantity(),
+	];
+}
+
+// установка кастомной цены в корзине
+$basketItem->setFields([
+	'PRICE' => 500, 
+	'CUSTOM_PRICE' => 'Y',
+]);
+
+// Получить все поля элемента корзины
+$arFields = $basketItem->getAllFields();
+foreach ($arFields as $fieldkey => $fieldname) {
+	$arr[$fieldkey] = $basketItem->getField($fieldname);
+}
